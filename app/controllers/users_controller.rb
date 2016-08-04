396b8590360d27a2f5c7efcee07db8b81213dc3c
kudo kanami <kycu.maz@gmail.com>
2016-08-04 16:54:07 +0000

    Finish user follow / unfollow actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,20 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         @microposts = @user.microposts.paginate(page: params[:page])
       end
+      
+      def following
+        @title ="Following"
+        @user = User.find(params[:id])
+        @users = @user.followed_users.paginate(page: params[:page])
+        render 'show_follow'
+      end
+      
+      def followers
+        @title = "Followers"
+        @user = User.find(params[:id])
+        @users = @user.followers.paginate(page: params[:page])
+        render 'show_follow'
+      end
 
   private
   
