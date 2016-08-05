46b431b65bab38fbd4cc5d6c02e0ee782977a5b9
kudo kanami <kycu.maz@gmail.com>
2016-08-05 11:56:29 +0000

    Finish user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  def feed
-    Micropost.where("user_id = ?", id)
-  end
+   def feed
+    Micropost.from_users_followed_by(self)
+   end
   
   def following?(other_user)
     relationships.find_by(followed_id: other_user.id)
